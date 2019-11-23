c9993b6fbf8e83a85d77008f055c35dfe7519f89
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-11-23 16:56:44 -0300

    Capture video instantly

--- src/dec.py
+++ src/dec.py
@@ -15,7 +15,7 @@ def get_parser():
     parser = argparse.ArgumentParser(
         description="Retrieve WAV audio data from an image bit plane.")
     parser.add_argument("enc_image", type=str, 
-                        help="File name (with path) of an image with audio encoded")
+                        help="File name (with path) of a PNG image with audio encoded")
     parser.add_argument("--n_of_channels", "-ch", type=int, choices=[1, 2], default=1, 
                         help="Number of audio channels (1=mono, 2=stereo)  (defaults to %(default)d)")
     parser.add_argument("--sample_rate", "-sr", type=int, choices=[8000, 44100], default=8000, 
@@ -36,7 +36,37 @@ def get_parser():
 
 def main(args):
     # TODO receive an image as input and return the hidden audio file
-    pass
+    enc_img = cv2.imread(args.enc_image)
+    if args.verbose:
+        print(f"enc_img.shape:", enc_img.shape)
+        print(f"enc_img.size:", enc_img.size)
+        print(f"enc_img.ndim:", enc_img.ndim)
+
+    if enc_img.ndim == 2:
+        pass
+    else:
+        enc_img = enc_img[..., 1] # get the green channel (all have the same message as of now)
+    
+    hidden_plane_img = get_bit_plane(img=enc_img, plane=args.bit_plane)
+    assert hidden_plane_img.dtype == np.uint8
+    decoded_audio = convert(hidden_plane_img.flatten(), to='int16')
+    if args.n_of_channels == 2:
+        # FIXME convert decoded_audio to a 2D array if it's stereo
+        warnings.warn("\nWarning: stereo audio isn't currently supported")
+    
+    fname, _ = os.path.splitext(os.path.basename(args.enc_image))
+    fname = os.path.join(args.output_folder, fname + "-decoded")
+    wavfile.write(filename=fname + ".wav", rate=args.sample_rate, data=decoded_audio)
+    if args.verbose:
+        print(f"Saved audio to '{fname}.wav'")
+    
+    if args.playback_audio:
+        if args.verbose:
+            print(f"Playing audio (~{decoded_audio.size // args.sample_rate}s)..")
+        # sd.play(decoded_audio, args.sample_rate)
+        # sd.wait() # wait until it is done playing
+        if args.verbose:
+            print("done.")
 
 ###############################################################################
 
--- src/enc.py
+++ src/enc.py
@@ -10,6 +10,7 @@ import numpy as np
 import sounddevice as sd
 from scipy.io import wavfile
 
+from codec import *
 from bitplane import set_bit_plane_partial
 from converter import convert
 
@@ -27,149 +28,153 @@ def get_parser():
                         help="Output folder to store the saved image frames  (defaults to '%(default)s/')")
     parser.add_argument("--save_audio", "-a", action="store_true", 
                         help="Save the audio file retrieved from the image as well")
-    parser.add_argument("--no_wait", "-n", action="store_true", 
-                        help="Don't wait for a key press to starting saving frames")
+    parser.add_argument("--grayscale", "-g", action="store_true", 
+                        help="Use grayscale frames instead of RGB")
+    parser.add_argument("--wait", "-w", action="store_true", 
+                        help="Wait for a key press to save frames")
     parser.add_argument("--verbose", "-v", action="store_true", 
                         help="Increase verbosity")
     return parser
 
 ###############################################################################
 
-def main(args):
-    # mic setup
-    device_info = sd.query_devices(kind='input')
+def setup_camera(args):
     if args.verbose:
         print("device_info:")
-        pprint.pprint(device_info)
-
-    # camera setup
-    start = time.time()
-    try:
-        cap = cv2.VideoCapture(0) # NOTE this may take a few seconds
-        if not cap.read()[0]:
-            raise Exception
-        FRAME_DELAY_MS = 10
-    except:
-        print("No camera found")
-        # cap = cv2.VideoCapture('kojima.png')
-        FRAME_DELAY_MS = 250
-        exit() # TODO allow static image use by passing an image file
+        pprint.pprint(sd.query_devices(kind='input'))
+        
+        start = time.time()
+    
+    # DirectShow (via videoInput)
+    cap = cv2.VideoCapture(index=0, apiPreference=cv2.CAP_DSHOW)
+    
     if args.verbose:
         print(f"Camera setup took {(time.time() - start):.1f}s")
 
-    height, width = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    hidden_plane_img = np.zeros((height, width), dtype='uint8')
+    # NOTE cv2.VideoCapture(0) may take a few seconds
+    if not cap.read()[0]:
+        raise Exception("No camera found")
+
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    depth  = 1 if args.grayscale else 3
+    if depth == 1:
+        warnings.warn("\nWarning: grayscale video isn't currently supported")
+    
+    return cap, height, width, depth
+
+def save_frame(__frame, message_uint8, stream, args):
+    if args.wait:
+        # FIXME check if a key (e.g. space) was pressed to save this finished frame
+        warnings.warn("\nWarning: waiting for key press isn't currently supported")
+    
+    fname = f"{time.strftime('%Y%m%d-%H%M%S')}_{stream._samplerate:.0f}_{stream._channels:.0f}"
+    fname = os.path.join(args.output_folder, fname)
+
+    cv2.imwrite(filename=fname + ".png", img=__frame)
     if args.verbose:
-        print("hidden_plane_img.shape:", hidden_plane_img.shape)
-        print("hidden_plane_img.size:", hidden_plane_img.size)
+        print(f"Saved image to '{fname}.png'")
 
-    # audio input stream setup
-    in_data_list = [] # list to store the audio blocks (in_data) captured by sd.InputStream
+    # TODO get audio from __frame instead of message_uint8
+    if args.save_audio:
+        #decoded_audio = decode(__frame, args.bit_plane)
+        decoded_audio = message_uint8
+        decoded_audio = convert(decoded_audio, to='int16')
+        
+        if stream._channels == 2:
+            # FIXME convert decoded_audio to a 2D array if it's stereo
+            warnings.warn("\nWarning: stereo audio isn't currently supported")
+        
+        wavfile.write(filename=fname + ".wav", rate=int(stream._samplerate), data=decoded_audio)
+        if args.verbose:
+            print(f"Saved audio to '{fname}.wav'")
 
-    # this is called (from a separate thread) for each audio block
+def main(args):
+    if args.n_of_channels == 2:
+        warnings.warn("\nWarning: stereo audio isn't currently supported")
+    if args.grayscale:
+        warnings.warn("\nWarning: grayscale video isn't currently supported")
+    if args.wait:
+        warnings.warn("\nWarning: waiting for key press isn't currently supported")
+
+    cap, height, width, depth = setup_camera(args) # NOTE this takes around 20s
+    message_uint8 = np.zeros(dtype='uint8', shape=height * width * depth // 8 )
+    if args.verbose:
+        print(f"(height, width, depth)=({height}, {width}, {depth})")
+        print(f"message_uint8: shape={message_uint8.shape} | size={message_uint8.size}")
+
+    # list to store the audio blocks captured by the audio input stream
+    in_data_list = []
     def in_stream_callback(in_data, frames, time, status):
         if status:
             print(status, file=sys.stderr if not args.verbose else sys.stdout)
-        in_data_list.append(np.copy(in_data))
+        in_data_list.append(np.copy(in_data)) # append audio block
     
-    # NOTE the number of frames passed to the stream callback can be set with the blocksize param
-    #      the special value blocksize = 0 (which is the default) may be used to request that the 
-    #      stream callback will receive an optimal (and possibly varying) number of frames 
-    stream = sd.InputStream(
-        channels=args.n_of_channels, 
-        samplerate=args.sample_rate, 
-        blocksize=0, 
-        dtype='int16',
-        callback=in_stream_callback)
+    stream = sd.InputStream(dtype='int16',
+                            channels=args.n_of_channels,
+                            samplerate=args.sample_rate,
+                            callback=in_stream_callback)
     if args.verbose:
         print("sample rate:", stream._samplerate)
-        print("number of audio channels:", stream._channels, "(mono)" if stream._channels == 1 else "stereo")
+        print("number of audio channels:", stream._channels, 
+              "(mono)" if stream._channels == 1 else "(stereo)")
 
     with stream: # listen for live audio input
-
-        hidden_bits = 0 # next bit to write to (indexed on the flattened hidden_plane_img)
-        
-        done = False # True if hidden_plane_img is full
-
+        hidden_bytes, done, __frame = 0, False, None
+        a = False
         while cap.isOpened():
-            ret, frame = cap.read()
-            if ret:
+            ret, frame = cap.read() # get image from camera
+            if not ret:
+                print(f"cap.read() returned {ret}", 
+                      file=sys.stderr if not args.verbose else sys.stdout)
+                break
+            else:
                 if cv2.waitKey(FRAME_DELAY_MS) & 0xFF == ord('q'):
                     break
 
-                # __frame = set_bit_plane_partial(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), args.bit_plane, 
-                #                                 hidden_plane_img, changed_bits=hidden_bits)            
-                __frame = np.copy(frame)
-                for ch in [0, 1, 2]:
-                    __frame[..., ch] = set_bit_plane_partial(frame[..., ch], args.bit_plane, 
-                                                             hidden_plane_img, changed_bits=hidden_bits)
-
-                cv2.imshow('frame', __frame)
+                if a: a = not a
 
                 if done:
-                    if not args.no_wait:
-                        # FIXME check if a key (e.g. space) was pressed to save this finished frame
-                        warnings.warn("\nWarning: current behaviour is the same as when using --no_wait")
-                        continue
+                    save_frame(__frame, message_uint8, stream, args)
+                    hidden_bytes, done = 0, False # reset values
+                    a = True
 
-                    fname = f"{time.strftime('%Y%m%d-%H%M%S')}_{stream._samplerate:.0f}_{stream._channels:.0f}"
-                    fname = os.path.join(args.output_folder, fname)
-                    cv2.imwrite(filename=fname + ".png", img=__frame)
-                    if args.verbose:
-                        print(f"Saved image to '{fname}.png'")
-
-                    if args.save_audio:
-                        # TODO get audio from __frame instead of hidden_plane_img
-                        assert hidden_plane_img.dtype == np.uint8
-                        decoded_audio = convert(hidden_plane_img.flatten(), to='int16')
-                        if stream._channels == 2:
-                            # FIXME convert decoded_audio to a 2D array if it's stereo
-                            warnings.warn("\nWarning: stereo audio isn't currently supported")
-                        wavfile.write(filename=fname + ".wav", rate=int(stream._samplerate), data=decoded_audio)
-                        if args.verbose:
-                            print(f"Saved audio to '{fname}.wav'")
-                    
-                    # reset values
-                    hidden_bits, done = 0, False
+                __frame = encode(frame, args.bit_plane, message_uint8, debug=a)
+                cv2.imshow('frame', __frame)
                 
                 # get all audio blocks that have been captured since the last loop iteration
                 stored_audio_blocks, in_data_list = in_data_list, []
-                if len(stored_audio_blocks) == 0:
-                    if args.verbose:
-                        print("No audio blocks stored in the last iteration "
-                              "(you may consider changing blocksize or samplerate)")
-                else:
+
+                if len(stored_audio_blocks) > 0:
                     # concatenate the stored audio blocks (uint16)
                     in_audio = np.concatenate(stored_audio_blocks)
-                    assert in_audio.dtype == np.int16
-                    __audio = convert(in_audio.reshape(-1), to='uint8')
-                    length = __audio.size
 
-                    if hidden_bits + length < hidden_plane_img.size:
-                        hidden_plane_img.ravel()[hidden_bits : hidden_bits + length] = __audio
-                        hidden_bits += length
+                    audio_uint8 = convert(in_audio.reshape(-1), to='uint8')
+                    length = audio_uint8.size
+
+                    if hidden_bytes + length < message_uint8.size:
+                        message_uint8.ravel()[hidden_bytes : hidden_bytes + length] = audio_uint8
+                        hidden_bytes += length
                     else:
-                        max_length = hidden_plane_img.size - hidden_bits
+                        max_length = message_uint8.size - hidden_bytes
                         if args.verbose:
-                            print(f"Hiding {max_length} bits (length={length} but "
-                                  f"hidden_bits={hidden_bits} and hidden_plane_img.size={hidden_plane_img.size})")
+                            print(f"Hiding {max_length} bytes (audio_uint8.size={length} but "
+                                  f"hidden_bytes={hidden_bytes} and message_uint8.size={hidden_plane_img.size})")
                         
                         # TODO verify if we can add partial blocks, i.e.:
-                        hidden_plane_img.ravel()[hidden_bits : ] = __audio[ : max_length]
+                        message_uint8.ravel()[hidden_bytes : ] = audio_uint8[ : max_length]
 
-                        # TODO save the audio data that didn't fit in hidden_plane_img
-                        #      to hide it in the next frame (__audio[max_length : ])
-                        
+                        # TODO save the audio data that didn't fit in message_uint8
+                        #      to hide it in the next frame (audio_uint8[max_length : ])
                         done = True
-            else:
-                print(f"cap.read() returned {ret}", file=sys.stderr if not args.verbose else sys.stdout)
-                break
         
         cap.release()
         cv2.destroyAllWindows()
 
 ###############################################################################
 
+FRAME_DELAY_MS = 10
 if __name__ == '__main__':
     args = get_parser().parse_args()
+    args.verbose = True # FIXME remove after testing
     main(args)
